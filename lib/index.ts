c0e084fa34d27b2700850f20af7ccab9952a6ea4
Pavel Strunkin <pavel.strunkin@lottoland.com>
2020-07-21 18:23:04 +0200

    update

--- lib/index.ts
+++ lib/index.ts
@@ -1,45 +1,59 @@
-import { Helper } from 'codeceptjs'
+import { Helper } from 'codeceptjs';
 import { PlaywrightVisualRegressionTracker, Config } from '@visual-regression-tracker/agent-playwright'
-import { DRIVERS } from './interfaces'
+import { DRIVERS, TrackOptions } from './interfaces'
+import { unlink, unlinkSync } from 'fs';
+import { join } from 'path';
+import { Build, VisualRegressionTracker, TestRun, } from '@visual-regression-tracker/sdk-js';
 
-export class VisualRegressionTrackerHelper extends Helper {
-  private config: Config;
-  private helper: any;
-
+class VisualRegressionTrackerHelper extends Helper {
+  private vrt: VisualRegressionTracker;
 
   constructor(config: Config) {
-    super()
-    this.config = config
-    this._setHelper();
+    super();
+    this.vrt = new VisualRegressionTracker(config)
   }
 
   /**
     * @param name {String} name of the page you want to track
     * @param diffTollerancePercent {Number} set acceptable difference from baseline, between `0-100`. Default `1`
      */
-  async track(name: string, diffTollerancePercent?: number) {
-    const page = this.helper.page;
-    const browser = this.helper.browser;
-    const fullPageScreenshots = this.helper.fullPageScreenshots;
+  async track(name: string, options: TrackOptions) {
 
-    const vrt = new PlaywrightVisualRegressionTracker(this.config, browser)
+    const helper = this._getHelper()
+    const filepath = join(__dirname, `${name}-${Date.now()}.png`);
+    const image = await helper.saveScreenshot(filepath, options.fullScreen)
+    unlinkSync(filepath)
 
-    return vrt.track(page, name, {
-      diffTollerancePercent,
-      screenshotOptions: {
-        fullPage: fullPageScreenshots
-      }
-    });
+    return this.vrt.track({
+      name,
+      imageBase64: image.toString("base64"),
+      ...options
+    })
   }
 
-  private _setHelper() {
+
+  _getHelper(): any {
+    if (this.helpers[DRIVERS.Puppeteer]) {
+      return this.helpers[DRIVERS.Puppeteer];
+    }
+    if (this.helpers[DRIVERS.WebDriver]) {
+      return this.helpers[DRIVERS.WebDriver];
+    }
+    if (this.helpers[DRIVERS.Appium]) {
+      return this.helpers[DRIVERS.Appium];
+    }
+    if (this.helpers[DRIVERS.WebDriverIO]) {
+      return this.helpers[DRIVERS.WebDriverIO];
+    }
+    if (this.helpers[DRIVERS.TestCafe]) {
+      return this.helpers[DRIVERS.TestCafe];
+    }
     if (this.helpers[DRIVERS.Playwright]) {
-      this.helper = this.helpers[DRIVERS.Playwright];
-      return;
+      return this.helpers[DRIVERS.Playwright];
     }
 
-    throw new Error(
-      `Not suported Helper`
-    );
+    throw new Error('Not supported driver');
   }
 }
+
+export default VisualRegressionTrackerHelper
--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,3 +1,19 @@
+import { type } from "os";
+
 export enum DRIVERS {
-    Playwright = 'Playwright'
+    Playwright = 'Playwright',
+    Puppeteer = 'Puppeteer',
+    WebDriver = 'WebDriver',
+    Appium = 'Appium',
+    WebDriverIO = 'WebDriverIO',
+    TestCafe = 'TestCafe',
+}
+
+export interface TrackOptions {
+    diffTollerancePercent?: number;
+    os?: string;
+    browser?: string;
+    viewport?: string;
+    device?: string;
+    fullScreen?: boolean;
 }
